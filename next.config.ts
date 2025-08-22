2ec7704886cd421c9d0bd659124f80c5f6361c5a
bharathkv <bharath@dreamteamsolutions.in>
2025-08-22 10:38:32 +0530

    fix: update Next.js configuration for custom build directory and improve tsconfig formatting

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  distDir: 'out', // Specify 'out' as the custom build directory
 };
 
 export default nextConfig;
